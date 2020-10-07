97e7eab371c41f6ba82f23fcabc5a772d8e8b171
MugiChu <mugichu29@gmail.com>
2020-10-07 22:55:18 +0700

    spboot

--- todo-list/src/main/java/com/ToDo/todolist/TodoListApplication.java
+++ todo-list/src/main/java/com/ToDo/todolist/TodoListApplication.java
@@ -1,13 +1,27 @@
 package com.ToDo.todolist;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @SpringBootApplication
 public class TodoListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
-	}
 
+		String url = "jdbc:postgresql://localhost:5432/todo_base";
+		String user = "";
+		String pass = "";
+		try {
+			Connection connection = DriverManager.getConnection(url, user, pass);
+
+			connection.close();
+		} catch (
+				Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 }
+
+
